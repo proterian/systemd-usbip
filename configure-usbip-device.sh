3d320a0623e2105f87d94e79434baefec3771455
proterian <lrpetrosian@gmail.com>
2023-09-07 12:47:44 +0300

    Add attach
    Add attach and detach its fix some Windows client

--- configure-usbip-device.sh
+++ configure-usbip-device.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -e
 BUS_ID=`/usr/sbin/usbip list -p -l | grep -i "#usbid=${USB_IDVENDOR}:${USB_IDPRODUCT}#" | cut '-d#' -f1`
 [[ -z "$BUS_ID" ]] || /usr/sbin/usbip bind --$BUS_ID
-
+sleep 1
+usbip attach --remote=localhost --$BUS_ID
+sleep 3
+usbip port
+sleep 3
+usbip detach --port=00
