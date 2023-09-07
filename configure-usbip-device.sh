6c23bf10ed391e3ae4b33542d3d40c68be1aa5c1
proterian <lrpetrosian@gmail.com>
2023-09-07 12:48:33 +0300

    Add Path
    Add path

--- configure-usbip-device.sh
+++ configure-usbip-device.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
+PATH=/etc:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 BUS_ID=`/usr/sbin/usbip list -p -l | grep -i "#usbid=${USB_IDVENDOR}:${USB_IDPRODUCT}#" | cut '-d#' -f1`
 [[ -z "$BUS_ID" ]] || /usr/sbin/usbip bind --$BUS_ID
 sleep 1
