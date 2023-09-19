fbea6b68a67f873a6276feb555e972d8215e6e92
proterian <lrpetrosian@gmail.com>
2023-09-19 19:26:26 +0300

    Update configure-usbip-device.sh

--- configure-usbip-device.sh
+++ configure-usbip-device.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 PATH=/etc:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
-BUS_ID=`/usr/sbin/usbip list -p -l | grep -i "#usbid=${USB_IDVENDOR}:${USB_IDPRODUCT}#" | cut '-d#' -f1`
+#BUS_ID=`/usr/sbin/usbip list -p -l | grep -i "#usbid=${USB_IDVENDOR}:${USB_IDPRODUCT}#" | cut '-d#' -f1`
+BUS_ID=`/usr/bin/usbip list -p -l | grep -i "busid=${USB_ID}" | cut '-d#' -f1`
 [[ -z "$BUS_ID" ]] || /usr/sbin/usbip bind --$BUS_ID
 sleep 1
 usbip attach --remote=localhost --$BUS_ID
