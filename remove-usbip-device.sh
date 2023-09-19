76305cc8ada6cd4b603e5a2d31c22ee6c3922d0d
proterian <lrpetrosian@gmail.com>
2023-09-19 19:26:49 +0300

    Update remove-usbip-device.sh

--- remove-usbip-device.sh
+++ remove-usbip-device.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-BUS_ID=`/usr/sbin/usbip list -p -l | grep -i "#usbid=${USB_IDVENDOR}:${USB_IDPRODUCT}#" | cut '-d#' -f1`
+#BUS_ID=`/usr/sbin/usbip list -p -l | grep -i "#usbid=${USB_IDVENDOR}:${USB_IDPRODUCT}#" | cut '-d#' -f1`
+BUS_ID=`/usr/bin/usbip list -p -l | grep -i "busid=${USB_ID}" | cut '-d#' -f1`
 [[ -z "$BUS_ID" ]] || /usr/sbin/usbip unbind --$BUS_ID
 
